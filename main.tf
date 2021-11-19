a69c8cf6efb509fc703271120e3f61072cc97833
liuyuan <709503583@qq.com>
2021-11-19 17:02:09 +0800

    Removes the provider setting and improves the Readme

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/mongodb"
-}
 locals {
   this_instance_id      = var.existing_instance_id != "" ? var.existing_instance_id : concat(alicloud_mongodb_instance.this.*.id, [""])[0]
   create_more_resources = var.existing_instance_id != "" || var.create ? true : false
--- modules/mongodb-3.4-rocksdb/main.tf
+++ modules/mongodb-3.4-rocksdb/main.tf
@@ -1,10 +1,3 @@
-provider "alicloud" {
-  profile                 = var.profile != "" ? var.profile : null
-  shared_credentials_file = var.shared_credentials_file != "" ? var.shared_credentials_file : null
-  region                  = var.region != "" ? var.region : null
-  skip_region_validation  = var.skip_region_validation
-  configuration_source    = "terraform-alicloud-modules/mongodb"
-}
 locals {
   engine_version = "3.4"
   storage_engine = "RocksDB"
